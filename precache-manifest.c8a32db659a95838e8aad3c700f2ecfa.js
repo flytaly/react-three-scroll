1748fb8444fee4162831ef2df93b4d836d8e069f
Vitaly Yerofeyevsky <flytaly@gmail.com>
2020-02-15 23:33:05 +0300

    Updates

---
+++ precache-manifest.c8a32db659a95838e8aad3c700f2ecfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c55cf8331cc6b50ae55550972f01a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1984a3ca1e3a3f9a062",
+    "url": "/static/css/main.dcf32a6b.chunk.css"
+  },
+  {
+    "revision": "bb6fb191e913a7817fc1",
+    "url": "/static/js/2.6c552e36.chunk.js"
+  },
+  {
+    "revision": "1bc405a2f9ca5b8136d11cf29f9b2788",
+    "url": "/static/js/2.6c552e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1984a3ca1e3a3f9a062",
+    "url": "/static/js/main.e6b46ce8.chunk.js"
+  },
+  {
+    "revision": "be01db2c13f5cbb2aff6",
+    "url": "/static/js/runtime-main.99e160be.js"
+  }
+]);
